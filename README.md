00cecf65de3ac9359024b902435a80dbc7f55212
Saifallak <Saifallak@yahoo.com>
2022-11-03 06:14:45 +0200

    mailing stuff.

---
+++ README.md
@@ -0,0 +1,41 @@
+<p align="center"><a href="https://lamaryarasoft.com/" target="_blank"><img src="./assets/images/logo.svg" width="400"></a></p>
+
+UI Made by [misaraadel](https://github.com/misaraadel) and Supported By [AQuadic](https://aquadic.com)
+
+----------
+
+# Getting started
+
+## Important Links
+
+- [AQuadic](https://aquadic.com)
+- [misaraadel](https://github.com/misaraadel)
+- [Live Site](https://lamaryarasoft.com/)
+
+## Script Requirement
+
+This script assumes you have the requirement as the following:
+
+- Just working browser to view.
+
+## Installation
+
+Clone the repository
+
+    git clone https://github.com/misaraadel/lamar
+
+Switch to the repo folder
+
+    cd lamar
+
+Also Need PHP Mailer download [PHP Mailer](https://github.com/PHPMailer/PHPMailer) (Tested with [v6.6.5](https://github.com/PHPMailer/PHPMailer/releases/tag/v6.6.5))
+
+    // unzip php mailer to folder named `PHPMailer`
+
+----------
+
+## Made with ♥ By
+
+<p align="center"><a href="https://AQuadic.com" target="_blank"><img src="https://AQuadic.com/img/logo.svg" width="200"></a></p>
+
+<p align="center"><a href="https://github.com/misaraadel" target="_blank"><img src="https://github.com/misaraadel/sonbola/blob/main/misara_logo.svg" width="200"></a></p>
---
+++ mailer.php
@@ -0,0 +1,62 @@
+<?php
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
+
+// Instantiation and passing [ICODE]true[/ICODE] enables exceptions
+$mail = new PHPMailer(true);
+
+
+$data = [
+    'name' => $_POST['name'],
+    'email' => $_POST['email'],
+    'phone' => $_POST['phone'],
+    'message' => $_POST['message'],
+];
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0; // Enable verbose debug output
+    $mail->isSMTP(); // Set mailer to use SMTP
+    $mail->Host = 'p3plzcpnl489472.prod.phx3.secureserver.net'; // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true; // Enable SMTP authentication
+    $mail->Username = 'mailer@lamaryarasoft.com'; // SMTP username
+    $mail->Password = '(fYf6e=QbyA0'; // SMTP password
+    $mail->SMTPSecure = 'tls'; // Enable TLS encryption, [ICODE]ssl[/ICODE] also accepted
+    $mail->Port = 587; // TCP port to connect to
+    $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
+
+    //Recipients
+    $mail->setFrom($data['email'], $data['email']);
+    $mail->addAddress('info@lamaryarasoft.com', 'Info');
+    $mail->addReplyTo($data['email'], $data['email']);
+
+    // Content
+    $mail->isHTML(true); // Set email format to HTML
+    $mail->Subject = 'You have new Contact Request ' . $data['email'];
+    $mail->Body = "Hello, my name is: <strong>{$data['name']}</strong><br>
+                    My Email is: <strong>{$data['email']}</strong><br>
+                    My Phone is: <strong>{$data['phone']}</strong><br>
+                    Message: <strong>{$data['message']}</strong>";
+
+    $mail->AltBody = "Hello, my name is: {$data['name']}
+                    My Email is: {$data['email']}
+                    My Phone is: {$data['phone']}
+                    Message: {$data['message']}";
+
+    $mail->send();
+
+    echo 'Message has been sent';
+    http_response_code(200);
+} catch (Exception $e) {
+//    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    http_response_code(500);
+}
+
+header("Location: https://lamaryarasoft.com");
+die();
